87527943c11739dc0ebe75a1956d1fccf676fecf
Aaron Mehrings <mehringsaaron@gmail.com>
2018-05-08 19:21:21 -0500

    latest with modal

--- SaveAz/src/app/item-detail/item-detail.component.ts
+++ SaveAz/src/app/item-detail/item-detail.component.ts
@@ -1,6 +1,6 @@
 
 import {Component, OnInit} from '@angular/core';
-
+import { ImageService } from '../image/shared/image.service';
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -17,19 +17,19 @@ export class ItemDetailComponent implements OnInit {
 
   open(content) {
     this.modalService.open(content).result.then((result) => {
-      this.closeResult = `Closed with: ${result}`;
+      //this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
-      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      //this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
     });
   }
 
-  private getDismissReason(reason:any): string {
-    if (reason === ModalDismissReasons.ESC) {
+  /*private getDismissReason(reason:any): string {
+    /*if (reason === ModalDismissReasons.ESC) {
       return 'by pressing ESC';
     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
       return 'by clicking on a backdrop';
     } else {
       return  `with: ${reason}`;
     }
-  }
+}*/
 }
