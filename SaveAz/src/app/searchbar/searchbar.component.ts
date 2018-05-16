891ecc2d0080a7194131848d93d9ed87ec4c444f
artur oganezov <artur@loop-depaulsecure-253-21.depaulsecure-student.depaul.edu>
2018-05-15 20:01:21 -0500

    progressss

--- SaveAz/src/app/searchbar/searchbar.component.ts
+++ SaveAz/src/app/searchbar/searchbar.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Http, Response, RequestOptions, Headers } from '@angular/http';
+import { ImageService } from '../image/shared/image.service'
 
 @Component({
   selector: 'app-searchbar',
@@ -7,21 +8,35 @@ import { Http, Response, RequestOptions, Headers } from '@angular/http';
   styleUrls: ['./searchbar.component.css']
 })
 
-export class SearchbarComponent{
-  apiRoot: string = "http://itunes.apple.com/search"; 
-  results:Object[];
+export class SearchbarComponent {
+  apiRoot: string = "https://api.walmartlabs.com/v1/search?apiKey=chwytn7wg8t344pf7eebg6sd&numItems=1&query=";
+  results: Object[];
   loading: boolean;
+  searchSubject: string;
 
-  constructor(private http: Http) { 
-    this.results =[];
+  constructor(private http: Http) {
+    this.results = [];
     this.loading = false;
-  }  
-
-  search(){
-    console.log();
-    let url = '$(this.apiRoot)/get';
-    this.http.get(url).subscribe(res => console.log(res.text()));
-
+    this.searchSubject = "";
   }
 
+  search() {
+    console.log();
+    this.searchSubject = document.getElementById("searchSubject").value;
+    let url = this.apiRoot + this.searchSubject;
+    if (this.searchSubject != "") {
+      var x = this.http.get(url).subscribe(res => console.log(res.json()));
+      var numItems = x.items.length;
+      for (var i = 0; i < numItems; i++) {
+        ImageService y = new ImageService();
+        y.visibleImages = []
+
+      }
+    }
+  }
 }
+
+// chwytn7wg8t344pf7eebg6sd
+// http://api.walmartlabs.com/v1/search?apiKey=chwytn7wg8t344pf7eebg6sd&&query=
+
+
