61a339b3303cfd5154b1c499c5dbfdb1db4a6dc2
artur oganezov <artur@arturs-MacBook-Pro.local>
2018-06-02 21:29:25 -0500

    some firebase files

--- SaveAz/src/app/alerts/alerts.component.spec.ts
+++ SaveAz/src/app/alerts/alerts.component.spec.ts
@@ -1,9 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AlertsComponent } from './alerts.component';
-<<<<<<< HEAD
 import {ImageService} from '../image/shared/image.service';
-=======
 import { AuthService } from '../authService/auth.service';
 import { ImageService } from '../image/shared/image.service';
 import { LikedItemServiceService } from '../liked-item-service.service';
@@ -15,22 +13,18 @@ class MockAuthService extends AuthService {
     super(null);
   }
 }
->>>>>>> 01b93737c31ec8c7e1d574a97626d29ec499d925
 
 class MockImageService extends ImageService {
   constructor() {
     super(null);
   }
 }
-<<<<<<< HEAD
-=======
 
 class MockLikedItemService extends LikedItemServiceService {
   constructor() {
     super();
   }
 }
->>>>>>> 01b93737c31ec8c7e1d574a97626d29ec499d925
 
 describe('AlertsComponent', () => {
   let component: AlertsComponent;
@@ -38,10 +32,6 @@ describe('AlertsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-<<<<<<< HEAD
-      declarations: [ AlertsComponent ],
-      providers: [{provide: ImageService, useClass: MockImageService}]
-=======
       declarations: [AlertsComponent],
       providers: [
         { provide: AuthService, useClass: MockAuthService },
@@ -49,7 +39,6 @@ describe('AlertsComponent', () => {
         { provide: LikedItemServiceService, useClass: MockLikedItemService },
       ],
       imports: [NgbModule.forRoot()]
->>>>>>> 01b93737c31ec8c7e1d574a97626d29ec499d925
     })
     .compileComponents();
   }));
